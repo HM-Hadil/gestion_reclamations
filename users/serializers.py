09929b69146cfc820002a9363467700f4936003c
Hadil Hammami <hadilhammami670@gmail.com>
2025-03-05 03:13:55 +0100

    fixing probleme username serializer

--- users/serializers.py
+++ users/serializers.py
@@ -10,5 +10,16 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        # Générer un nom d'utilisateur à partir de l'email
+        validated_data['username'] = validated_data['email']
+        
+        # Créer l'utilisateur avec tous les champs requis
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            role=validated_data['role']
+        )
         return user
