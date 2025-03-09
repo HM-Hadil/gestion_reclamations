78ec17f08f7daad32f66783865a62b8ea4cc8787
Hadil Hammami <hadilhammami670@gmail.com>
2025-03-09 22:41:17 +0100

    validate the email with nom.prenom@isimg.tn

--- users/models.py
+++ users/models.py
@@ -3,25 +3,26 @@ from django.db import models
 from django.core.exceptions import ValidationError
 import re
 
+def validate_isimg_email(value):
+    """ Vérifie que l'email suit le format nom.prenom@isimg.tn """
+    email_pattern = r'^[a-zA-Z]+\.[a-zA-Z]+@isimg\.tn$'
+    if not re.match(email_pattern, value):
+        raise ValidationError("Seuls les emails au format 'nom.prenom@isimg.tn' sont autorisés.")
+
 class User(AbstractUser):
     ROLE_CHOICES = [
         ('responsable', 'Responsable'),
         ('technicien', 'Technicien'),
         ('enseignant', 'Enseignant'),
     ]
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.EmailField(unique=True)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(unique=True, validators=[validate_isimg_email])
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'role']
-
-    def clean(self):
-        super().clean()
-        email_pattern = r'^[a-zA-Z]+\.[a-zA-Z]+@isimg\.tn$'
-        if not re.match(email_pattern, self.email):
-            raise ValidationError({'email': "Seuls les emails au format 'firstname.lastname@isimg.tn' sont autorisés."})
 
     def __str__(self):
         return self.email
